847962c23421c6a4e783d1eb8bec2a46d6ba40b9
DKelly <kelly13493@gmail.com>
2019-10-03 23:07:20 -0400

    Adding rest framework and set url for api calls

--- taskManagement/scrumboard/serializers.py
+++ taskManagement/scrumboard/serializers.py
@@ -7,14 +7,14 @@ class CardSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Card
-
+        fields = '__all__'
 
 class ListSerializer(serializers.ModelSerializer):
     cards = CardSerializer(read_only=True, many=True)
 
     class Meta:
         model = List
-
+        fields = '__all__'
 
 
 
--- taskManagement/scrumboard/urls.py
+++ taskManagement/scrumboard/urls.py
@@ -4,7 +4,7 @@ from django.views.generic import TemplateView
 from .api import ListApi, CardApi
 
 urlpatterns = [
-    url(r'^lists$', ListApi.as_view()),
-    url(r'^cards$', CardApi.as_view()),
-    url(r'^home', TemplateView.as_view(template_name="scrumboard/home.html")),
+    url('lists$', ListApi.as_view()),
+    url('cards$', CardApi.as_view()),
+    url('home', TemplateView.as_view(template_name="scrumboard/home.html")),
 ]
